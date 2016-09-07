24cf618bc4e7625c08e513b1ea580e4cf6a49122
⚡ John Gracey ⚡ <jagracey@users.noreply.github.com>
2016-09-06 20:56:17 -0700

    Update producer.js

--- clients/producer.js
+++ clients/producer.js
@@ -8,27 +8,29 @@ var queue = process.argv.slice(2)[0] || 2;
 var request = require('request');
 var crypto = require('crypto');
 
-const NAME = crypto.createHash('md5').update( Math.random().toString() ).digest("hex");
+const NAME = crypto.createHash('md5')
+              .update( Math.random().toString() )
+              .digest("hex");
 var message_count = 0;
 
 function produceMessage(){
   var payload = NAME +'|'+ (message_count++) +'|'+ queue +'|';
 
-  console.log('Sending request: '+ payload);
+  console.log(`Sending request: ${payload}`);
 
     var options = {
-      url: 'http://localhost:3000/queues/'+ queue +'/messages',
+      url: `http://localhost:3000/queues/${queue}/messages`,
       method: 'POST',
       body: payload,
       headers: { 'Content-Type': 'text/plain' }
     };
     request.post(options,function (error, res, body) {
       if (error){
-        console.log(error,(res)?res.statusCode:null,body);
+        console.log(error, body, (res||{}).statusCode);
         return process.exit(1);           // Arbitrarily decide to exit if the Broker breaks....
       }
       console.log(body);
-      setTimeout(function(){ produceMessage(); }, DELAY);
+      setTimeout(produceMessage, DELAY);
     });
 
 }
